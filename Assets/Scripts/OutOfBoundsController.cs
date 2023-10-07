8da704494e689496cedac9eab4d84ac84126c9c0
Jeremias Poutanen <144674280+Jeremias-Poutanen@users.noreply.github.com>
2023-10-08 01:37:29 +0300

    Add out of bounds script

---
+++ Assets/Scripts/OutOfBoundsController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBoundsController : MonoBehaviour
+{
+    [SerializeField] Collider2D[] boundsCollider;
+    [SerializeField] GameObject respawnPoint;
+    PlayerController playerController;
+    GameManager gameManager;
+    void Start()
+    {
+        playerController = FindObjectOfType<PlayerController>();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void OutOfBoundsRespawn()
+    {
+        playerController.transform.position = respawnPoint.transform.position;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if(collider2D.tag == "Player")
+        {
+            playerController.transform.position = respawnPoint.transform.position;
+            gameManager.TakeDamage();
+        }
+    }
+}
