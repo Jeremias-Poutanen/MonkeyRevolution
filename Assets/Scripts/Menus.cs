064f46bc482bb5c789483fecfe36a92c3af6a03c
Jessica Koivunen <Jessica Koivunen@ictlab.local>
2023-10-08 00:09:19 +0300

    added main menu scene

--- Assets/Scripts/Menus.cs
+++ Assets/Scripts/Menus.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menus : MonoBehaviour
 {
-    public GameObject GameOverMenu;
-    public GameObject StartMenu;
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Concept");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        GameOverMenu.SetActive(false);
-        StartMenu.SetActive(true);
+
     }
 
     // Update is called once per frame
