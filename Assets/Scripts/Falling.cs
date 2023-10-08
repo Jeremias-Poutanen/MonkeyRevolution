6a7c3d76f153697b7dabe6dd5e6314b026159151
Jeremias Poutanen <144674280+Jeremias-Poutanen@users.noreply.github.com>
2023-10-08 15:00:49 +0300

    Update faling script

--- Assets/Scripts/Falling.cs
+++ Assets/Scripts/Falling.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class Falling : MonoBehaviour
 {
+
+    GameManager gameManager;
     public float moveamount = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.down * moveamount * Time.deltaTime;
-
     }
 }
